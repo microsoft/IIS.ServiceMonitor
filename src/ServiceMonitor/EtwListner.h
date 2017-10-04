42bcf087bc89888e8bca8d10cbe91e4adaf1fba8
Chiyuan Ma <chiyma@microsoft.com>
2017-10-04 15:32:11 -0700

    Code Clean Up

--- src/ServiceMonitor/EtwListner.h
+++ src/ServiceMonitor/EtwListner.h
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 
 #pragma once
 
@@ -18,5 +20,7 @@ public:
 
 	EtwListner();
 	~EtwListner();
+	void StartListen(LPWSTR pStrSessionName);
+
 
 };
